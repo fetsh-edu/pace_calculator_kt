40c3ad1c20dc34f49355e7dd30a593b5cbc61959
fetsh <fetshme@gmail.com>
2025-07-22 15:16:03 +0300

    feat: add domain model and update logic an first tests

--- app/src/main/java/me/fetsh/pacecalculator/ui/calculator/PaceCalculatorViewModel.kt
+++ app/src/main/java/me/fetsh/pacecalculator/ui/calculator/PaceCalculatorViewModel.kt
@@ -1,1 +1,23 @@
 package me.fetsh.pacecalculator.ui.calculator
+
+/**
+ * The core logic of the application. A pure function that takes the current
+ * state and a message, and returns a new state and a set of side effects.
+ *
+ * @param msg The event that has occurred.
+ * @param model The current state of the application.
+ * @return A Pair containing the new state and a set of effects to be executed.
+ */
+fun update(
+    msg: Msg,
+    model: Model,
+): Pair<Model, Set<Effect>> =
+    when (msg) {
+        is Msg.DistanceChanged -> {
+            model.copy(distance = msg.value) to emptySet()
+        }
+        is Msg.TimeChanged -> {
+            model.copy(time = msg.value) to emptySet()
+        }
+        else -> model to emptySet() // TODO: Implement other messages
+    }
